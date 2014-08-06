fd3ce2ebd2d28a2b425b5f837d683a55266cb231
Wil Wade <willmwade@gmail.com>
2014-08-06 12:52:11 -0400

    Show relation on Post

--- frontend/views/post/view.php
+++ frontend/views/post/view.php
@@ -12,7 +12,7 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="post-view">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+    <h1><?= Html::encode($this->user->username .': '. $this->title) ?></h1>
 
     <p>
         <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
@@ -31,7 +31,7 @@ $this->params['breadcrumbs'][] = $this->title;
             'id',
             'title',
             'content:ntext',
-            'user_id',
+            'user.username', //user_id
         ],
     ]) ?>
 
